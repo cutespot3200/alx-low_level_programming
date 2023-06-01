79dabaeb3663795d4f46da8adcc5fc02329101b6
cutespot3200 <id_bailey32@yahoo.com>
2023-06-01 12:16:19 -0700

    error fixed

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,7 @@
-#ifndef  LISTS_H
+#ifndef LISTS_H
+
 #define LISTS_H
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -13,9 +15,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
